566b5ac14011b99c6737c86986143e3251d0398a
Aleksei_Potapov <aleksei_potapov@epam.com>
2020-09-08 16:36:38 +0200

    add github reducer and context

---
+++ src/context/github/GithubState.js
@@ -0,0 +1,18 @@
+import React, {useReducer} from "react";
+import {GithubContext} from "./githubContext";
+import {githubReducer} from "./githubReducer";
+
+export const GithubState = ({children}) => {
+    const initialState = {
+        user: {},
+        users: [],
+        loading: false,
+        repos: []
+    }
+    const [state, dispatch]  = useReducer(githubReducer, initialState)
+    return (
+        <GithubContext.Provider value={{}}>
+            {children}
+        </GithubContext.Provider>
+    )
+}
