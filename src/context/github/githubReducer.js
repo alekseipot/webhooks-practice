638b52773ebc855625b6e7e907ed16604d9282e2
Aleksei_Potapov <aleksei_potapov@epam.com>
2020-09-08 16:43:23 +0200

    add github state

--- src/context/github/githubReducer.js
+++ src/context/github/githubReducer.js
@@ -4,8 +4,8 @@ const handlers = {
     [SEARCH_USERS]: (state, payload) => ({...state, users: payload, loading: false}),
     [GET_REPOS]: (state, payload) => ({...state, repos: payload, loading: false}),
     [GET_USER]: (state, payload) => ({...state, user: payload, loading: false}),
-    [SET_LOADING]: (state, payload) => ({...state, loading: true}),
-    [CLEAR_USERS]: (state, payload) => ({...state, users: []}),
+    [SET_LOADING]: (state) => ({...state, loading: true}),
+    [CLEAR_USERS]: (state) => ({...state, users: []}),
     DEFAULT: state => state
 }
 
