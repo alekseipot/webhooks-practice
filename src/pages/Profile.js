fbc28364846449f01408c2ce7a5d0129f1730b8c
Aleksei_Potapov <aleksei_potapov@epam.com>
2020-09-09 15:56:28 +0200

    modify profile page

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -1,9 +1,63 @@
-import React from "react";
+import React, {useContext, useEffect} from "react";
+import {GithubContext} from "../context/github/githubContext";
+import {Link} from "react-router-dom";
 
-export const Profile =() => {
+export const Profile = ({match}) => {
+    const {getUser, getRepos, loading, user, repos} = useContext(GithubContext)
+    const urlName = match.params.name
+
+    useEffect(() => {
+        getUser(urlName)
+        getRepos(urlName)
+        // eslint-disable-next-line
+    }, [])
+
+    if (loading) {
+        return <p className="text-center">Loading...</p>
+    }
+
+    const {
+        name, company, avatar_url, location, bio, blog,
+        login, html_url, followers, public_repos, public_gists, following
+    } = user
     return (
-        <div>
-            <h1>Profile page</h1>
-        </div>
+        <React.Fragment>
+            <Link to="/" className="btn btn-link">Back Home</Link>
+
+            <div className="card mb-4">
+                <div className="card-body">
+                    <div className="row">
+                        <div className="col-sm-3 text-center">
+                            <img src={avatar_url} alt={name} style={{width: 150}}/>
+                            <h1>{name}</h1>
+                            {location && <p>Location: {location}</p>}
+                        </div>
+                        <div className="col">
+                            {bio && <React.Fragment>
+                                <h3>BIO</h3>
+                                <p>{bio}</p>
+                            </React.Fragment>}
+                            <a href={html_url} target="_blank" rel="noopener noreferrer" className="btn btn-dark"> Open profile on github</a>
+                            <ul>
+                                {login && <li>
+                                    <strong>Username: </strong>{login}
+                                </li>}
+                                {company && <li>
+                                    <strong>Company: </strong>{company}
+                                </li>}
+                                {blog && <li>
+                                    <strong>Blog: </strong>{blog}
+                                </li>}
+                            </ul>
+                            <div className="badge badge-primary">Followers: {followers}</div>
+                            <div className="badge badge-success">Following: {following}</div>
+                            <div className="badge badge-info">Repositories: {public_repos}</div>
+                            <div className="badge badge-dark">Gists: {public_gists}</div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            {repos.join()}
+        </React.Fragment>
     )
 }
